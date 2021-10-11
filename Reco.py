0156718ea2291c023dbf1df1ca3dc988b809cdf2
shinhyeokkang <ksh84610@gmail.com>
2021-10-11 21:51:46 +0900

    w/o comment

--- Reco.py
+++ Reco.py
@@ -16,7 +16,7 @@ for gpu in gpus:
 model = tf.keras.models.load_model('cnn_model_i0003.h5')
 
 class Recommendation():
-    def newimage(self,img):
+    def reco_items(self,imgOrVal,New):
         client = MongoClient('mongodb://localhost:27017/')
         db = client.ItemDB
         collection = db.ItemCollections
@@ -24,48 +24,46 @@ class Recommendation():
         client.close()
         #수정중 데이터프레임으로바꺼야댐
         total_data = pd.DataFrame(list(results))
-        print(total_data)
+        #print(total_data)
         
-        print(total_data.isnull().values.any())
-        data = []
-        ImageFile.LOAD_TRUNCATED_IMAGES = True
-        Image.MAX_IMAGE_PIXELS = None
+        #print(total_data.isnull().values.any())
+        if New:
+            
+            data = []
+            ImageFile.LOAD_TRUNCATED_IMAGES = True
+            Image.MAX_IMAGE_PIXELS = None
+
+            image = imgOrVal
+            image = image.resize((224,224))
+            image = np.array(image, dtype='uint8')
+            if image.shape != (224,224,3):
+                if image.shape == (224,224,4):
+                    image = np.delete(image, 3, 2)
+                    print('before 224,224,4 to ', image.shape)
+                elif image.shape == (224,224):
+                    temp = image[np.newaxis].swapaxes(0,1).swapaxes(1,2)
+                    image = np.append(temp, temp, axis=2)
+                    image = np.append(image, temp, axis=2)
+                    print('before 224,224 to ', image.shape)
+            image = image/255
+            data.append(image)
+            data = np.array(data)
+
+            pred = model.predict(data)
+            print(pred[0],' Reco.Newimage Predict Complete!')
+            return self.cal_distance(pred[0],total_data)
         
-        image = img#Image.open('/home/jmkim/capstone/flask/uploads/tree.jpg')#받은이미지
-        
-        
-        testi = Image.open('/home/jmkim/capstone/flask/uploads/tree.jpg')
-        print(type(testi),"여기")
-        
-        
-        image = image.resize((224,224))
-        image = np.array(image, dtype='uint8')
-        if image.shape != (224,224,3):
-            if image.shape == (224,224,4):
-                image = np.delete(image, 3, 2)
-                print('before 224,224,4 to ', image.shape)
-            elif image.shape == (224,224):
-                temp = image[np.newaxis].swapaxes(0,1).swapaxes(1,2)
-                image = np.append(temp, temp, axis=2)
-                image = np.append(image, temp, axis=2)
-                print('before 224,224 to ', image.shape)
-        image = image/255
-        data.append(image)
-        data = np.array(data)
-        
-        pred = model.predict(data)
-        print(pred[0])
-        print('Done!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-        return self.recommend_items(pred[0],total_data)
+        else:
+            
+            return self.cal_distance(imgOrVal[0],total_data)
     
     
     
-    def recommend_items(self,value,data):
+    def cal_distance(self,value,data):
         re = data.copy()
         re['Distance'] = 0
-        print(value,"벨류야")
-        print(data.iloc[0:10,0:4])
         for i in range(0,len(data)):
-            re.loc[i,'Distance'] = distance.euclidean(value,re.iloc[i,0:4])
+            re.loc[i,'Distance'] = distance.euclidean(value,re.iloc[i,0:4])*10000
         re.sort_values(by=['Distance'], axis=0, inplace=True)
+        print('Reco Distance Calculation Complete!')
         return re
--- app/templates/main/index.py
+++ app/templates/main/index.py
@@ -6,6 +6,9 @@ from flask import current_app as app
 # 추가할 모듈이 있다면 추가
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
+from bson.json_util import dumps, loads
+import pandas as pd
+
 from PIL import Image
 from NCF import Ncf
 from Reco import Recommendation
@@ -14,35 +17,48 @@ from Reco import Recommendation
 
 # data 로드
 ncf = Ncf()
-reco = Recommendation()
+cnn = Recommendation()
 tmp_list = []
+list_results = []
+master_dict = {}
+df = pd.DataFrame(columns = ['0','1','2','3','Item_id','Item_url', 'Title', 'Company'])
  
 main= Blueprint('main', __name__, url_prefix='/')
  
 @main.route('/', methods=['GET'])
 def index():
     # /main/index.html은 사실 /project_name/app/templates/main/index.html을 가리킵니다.
-    return render_template('/main/index.html')
+    return render_template('/shop/shop.html')
 
 
 #파일 업로드 처리
 @main.route('/fileUpload', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-       
-        f = request.files['file']
-        #저장할 경로 + 파일명
+        
+        global df
+        df.drop_duplicates(inplace=True,ignore_index = True)
+        
+        #방사진 분석
+        f = request.files['file']#저장할 경로 + 파일명
         f.save(f'uploads/{secure_filename(f.filename)}') # /upload 폴더에 저장
-        print(f'uploads/{secure_filename(f.filename)}')
-        print(type(f))
-        print(f.filename)
         image = Image.open('/home/jmkim/capstone/flask/uploads/'+f.filename)
-        #temp = request.form['num']
-        #tmp_list = temp.split(",")
-        testlist = reco.newimage(image)
-        #recolist = ncf.reco_items(tmp_list, 30)
-        print(testlist)
-    return render_template('/shop/reco.html')#, data=recolist)
+        roomlist = list(cnn.reco_items(image,New=True).loc[:,'Item_url'])#리턴: Item_url
+        print(roomlist)
+        
+        
+        #item분석
+        item = request.form['itemlist']
+        item_id_list = list(map(int, item.split(" ")[1:]))#공백제거
+        #ncf
+        ncflist = ncf.reco_items(item_id_list, 30)#리턴: Item_id
+        #cnn
+        predVal = df[df['Item_id'].isin(item_id_list)].iloc[:,:4].values
+        cnnlist = list(cnn.reco_items(predVal,New=False).loc[:,'Item_url'])#리턴: Item_url 
+        
+
+        
+    return render_template('/shop/reco.html')#, ncf=ncflist,room=roomlist)
     
 @main.route('/json', methods = ['GET'])
 def json_file():
@@ -70,19 +86,38 @@ def query():
     client = MongoClient('mongodb://localhost:27017/')
     db = client.ItemDB
     collection = db.ItemCollections
-    results = collection.find({"Auto":queryName},{"_id":0,"Item_url":1, "Title":1, "Company":1,"Auto":1})
+    results = collection.find({"Auto":queryName},{'_id':0,'0':1,'1':1,'2':1,'3':1,'Item_id':1,'Item_url':1, 'Title':1, 'Company':1})
     client.close()
-    print(results)
-    list_results = list(results)
-    #for result in results:
-    #    list_results.append(result)    
+    #print(results, "find 결과 출력")
+    #NCF 및 CNN 학습용 Dict에 추가 (Json 형식 그대로 유지, 추후 delete시 검색 삭제 용이하게)
+
     
+    list_results = list(results)
+    #print(list_results, "리스트 씌운 결과\n")
+    '''
+    for mongo_doc in results:
+        # mongo_doc is a <class 'dict'> returned from the async mongo driver, in this acse motor / pymongo.
+        # result of executing a simple find() query.
+
+        json_string = dumps(mongo_doc)
+        # serialize the <class 'dict'> into a <class 'str'> 
+        back_to_dict = loads(json_string)
+        # to unserialize, thus return the string back to a <class 'dict'> with the original 'ObjectID' type.
+        master_dict.update(back_to_dict)
+        #데이터 프레임 생성
+
+    print(back_to_dict)
+    print(master_dict)
+    df = pd.DataFrame(back_to_dict)
+
+    '''
+    df_new = pd.DataFrame(list_results)
+    global df
+    df = pd.concat([df,df_new],ignore_index=True)
+    #print("DataFrame 입니다\n", df, "DataFrame 입니다\n")
+   
    # 사진파일 item_url로 검색해서 같이 보내줘야함 
-    #print(list_results[0])
-    #NCF 학습용 리스트에 추가
-    tmp_list.append(list_results[0]['Item_url'])
-    print(tmp_list)
-    #print(type(list_results))
+    
     return jsonify(list_results[0])
 
 @main.route('/shop', methods = ['GET'])
